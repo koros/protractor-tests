174464ca75433fff7d5f844ae28c473e23305cc4
Geoffrey Koros <gkoros@mobileaccord.com>
2017-11-24 17:07:31 +0300

    Add a few test cases for the login screen

--- secrets.js
+++ secrets.js
@@ -2,3 +2,8 @@ exports.login = {
   user: '',
   password: ''
 }
+
+exports.fakeAccount = {
+  user: 'fakeuser@gmail.fake',
+  password: 'fakePassword'
+}
--- spec.js
+++ spec.js
@@ -16,6 +16,21 @@ describe('Login', function() {
   		expect(browser.getTitle()).toEqual('Login');
   	});
 
+  	it('should promt for password if left blank', function() {
+		browser.driver.findElement(by.id('Password')).sendKeys('');
+		browser.driver.findElement(by.className('ui-button-large')).click().then(function() {
+	        expect(browser.driver.findElement(by.className('field-validation-error')).getText()).toEqual("The Password field is required.");
+	    }, 10000);
+  	});
+
+  	it('should fail to login when wrong credentials are entered', function() {
+		browser.driver.findElement(by.id('EmailAddress')).sendKeys(secrets.fakeAccount.user);
+		browser.driver.findElement(by.id('Password')).sendKeys(secrets.fakeAccount.password);
+		browser.driver.findElement(by.className('ui-button-large')).click().then(function() {
+	        expect(browser.driver.findElement(by.tagName('li')).getText()).toEqual("Incorrect username or password");
+	    }, 10000);
+  	});
+
   	it('should login', function() {
 		browser.driver.findElement(by.id('EmailAddress')).sendKeys(secrets.login.user);
 		browser.driver.findElement(by.id('Password')).sendKeys(secrets.login.password);
