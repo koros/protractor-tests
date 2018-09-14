fe6dc95e4a17bd0fd966eaffbc1170ef717d7faa
Elisha <elisha@geopoll.com>
2018-09-14 12:03:57 +0300

    Checks

--- app_stuff.js
+++ app_stuff.js
@@ -1,5 +1,9 @@
-// at the top of the test spec:
+// apping
 var fs = require('fs');
+var settings = require('./settings');
+var username = process.env.GP_USER;
+var password = process.env.GP_PASSWORD;
+
 
 beforeEach(function() {
 	browser.ignoreSynchronization = true;
@@ -12,7 +16,13 @@ afterEach(function() {
 describe('Live links', function() {
     it('should load Home', function() {
      browser.get('localhost:5000');
-     browser.driver.sleep(10000);     
+     browser.driver.sleep(2000);     
      expect(browser.getTitle()).toEqual('GeoPoll');
-    })
+    });
+
+    it('should register', function() { 
+    	browser.driver.findElement(by.className('mobilenumber form-control')).sendKeys(settings.fakeNumber.appNo);
+    	browser.driver.sleep(2000); 
+    	browser.driver.findElement(by.className('signup btn btn-default btn-block btn-lg')).click().then(function() {
+    });
 });
